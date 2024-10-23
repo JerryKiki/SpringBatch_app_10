cc44f8208b2aa59ce67fc928a52be3d0acd86630
JerryKiki <gasi758@gmail.com>
2024-10-23 14:19:43 +0900

    각종 주석 추가2 => 테스트 코드

--- src/test/java/com/lyj/proj/springbatch_app_10/controller/MemberControllerTests.java
+++ src/test/java/com/lyj/proj/springbatch_app_10/controller/MemberControllerTests.java
@@ -20,9 +20,14 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+//TDD (테스트 주도 방식 개발)
+
 @SpringBootTest
+//MocMvc 설정 / MocMvd => 실제 웹서버를 띄우지 않아도 HTTP요청을 흉내내서 Controller 메서드를 실행시켜줌
+//서버를 구동하지 않아도 잠깐 요청 보내는 척을 해주는 거임
+//Get이나 Post 요청을 이 테스트 환경에서 보낼 수 있다는 것
 @AutoConfigureMockMvc
-@Transactional
+@Transactional //테스트 메서드가 끝나면 다시 롤백
 @ActiveProfiles("test")
 public class MemberControllerTests {
     @Autowired
@@ -35,14 +40,15 @@ public class MemberControllerTests {
     void t1() throws Exception {
         // WHEN
         ResultActions resultActions = mvc
-                .perform(get("/member/join"))
+                .perform(get("/member/join")) //member/join에 get요청을 보냄 == form 내놔
                 .andDo(print());
         // THEN
         resultActions
-                .andExpect(status().is2xxSuccessful())
-                .andExpect(handler().handlerType(MemberController.class))
-                .andExpect(handler().methodName("showJoin"))
-                .andExpect(content().string(containsString("회원가입")));
+                .andExpect(status().is2xxSuccessful()) //200번대 == 성공, 성공으로 끝났는지 실패로 끝났는지?
+                .andExpect(handler().handlerType(MemberController.class)) //요청을 MemberContorller가 처리했는지?
+                .andExpect(handler().methodName("showJoin")) //실행된 Method가 showJoin인지?
+                .andExpect(content().string(containsString("회원가입"))); //해당 페이지에 "회원가입" 텍스트가 있는지?
+        //"Expect"이므로 한국어로 풀면 의문문이다.
     }
 
     @Test
@@ -50,19 +56,19 @@ public class MemberControllerTests {
     void t2() throws Exception {
         // WHEN
         ResultActions resultActions = mvc
-                .perform(post("/member/join")
-                        .with(csrf())
-                        .param("username", "user999")
+                .perform(post("/member/join") // member/join에 Post 요청을 보냄
+                        .with(csrf()) //csrf 토큰을 추가해서 보안 검증 통과를 하도록 함
+                        .param("username", "user999") //파라미터로 이것들을 넘기겠다(아래 3줄), 회원가입 데이터
                         .param("password", "1234")
                         .param("email", "user999@test.com")
                 )
                 .andDo(print());
         // THEN
         resultActions
-                .andExpect(status().is3xxRedirection())
+                .andExpect(status().is3xxRedirection()) //Redirection이 되었는지? (300번대)
                 .andExpect(handler().handlerType(MemberController.class))
                 .andExpect(handler().methodName("join"))
-                .andExpect(redirectedUrlPattern("/member/login?msg=**"));
-        assertThat(memberService.findByUsername("user999").isPresent()).isTrue();
+                .andExpect(redirectedUrlPattern("/member/login?msg=**")); //리다이렉트 Url 패턴이 이 형식인지?
+        assertThat(memberService.findByUsername("user999").isPresent()).isTrue(); //"memberService에서 user999를 찾았을 때 존재하니?"가 True인지
     }
 }
